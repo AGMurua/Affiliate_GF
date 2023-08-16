7a26ed7056adc4e9031438916bbd7170118fa020
AGMurua <78284924+AGMurua@users.noreply.github.com>
2023-08-16 19:25:44 -0300

    Removed layout files. Added new controllers

---
+++ AffiliatesApi/AffiliatesApi/Controllers/AffiliateController .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AffiliatesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AffiliateController : ControllerBase
+    {
+    }
+}
